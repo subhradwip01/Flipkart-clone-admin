3259fc08f2e9a80b1461b68e5d8486dbdba6c0bd
subhradwi01 <subhradwi01@gmail.com>
2023-02-07 23:59:10 +0530

    Home setup

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 import header from "../../assets/ecom-home.png"
+import { Container,Row, Col } from 'react-bootstrap'
 
 const Home = () => {
   return (
     <Layout>
-        <div style={{
+
+      <Container>
+      <Row>
+          <Col md={6}></Col>
+      </Row>
+      </Container>
+      
+        {/* <div style={{
           textAlign:"center",
           padding:"50px 30px"
         }}>
@@ -18,7 +26,8 @@ const Home = () => {
             fontWeight:300,
             fontSize:"20px"
           }}>Maintain your online store</p>
-        </div>
+        </div> */}
+        
     </Layout>
   )
 }
