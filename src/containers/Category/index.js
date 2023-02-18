d80dd3f3fd5adb92b3ffa1a3d171be296d64c861
subhradwi01 <subhradwi01@gmail.com>
2023-02-18 20:37:40 +0530

    category for created

--- src/containers/Category/index.js
+++ src/containers/Category/index.js
@@ -1,69 +1,133 @@
-import React, { useEffect } from 'react'
-import { Alert, Col, Row } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import { getCategory } from '../../actions/category.action'
-import Layout from '../../components/Layout'
-
+import React, { useEffect, useState } from "react";
+import { Alert, Col, Row, Button, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { addCategory, getCategory } from "../../actions/category.action";
+import Layout from "../../components/Layout";
+import CustomModal from "../../components/Modal";
+import Input from "../../components/Input";
 const Category = () => {
-   const dispatch = useDispatch(); 
-   const {categories, loading, error, errorMessage} = useSelector(state=>state.category)
-    useEffect(()=>{
-        dispatch(getCategory())
-    },[])
-  console.log(categories)
-  const renderCategories = (categories) =>{
+  const dispatch = useDispatch();
+  const { categories, loading, error, errorMessage } = useSelector(
+    (state) => state.category
+  );
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [parentId, setParentId] = useState(null);
+  const [categoryImage,setCategoryImage]=useState(null);
+  useEffect(() => {
+    dispatch(getCategory());
+  }, []);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const renderCategories = (categories) => {
     let rCategories = [];
-    for(let category of categories){
-        rCategories.push(
-            <li key={category._id}>
-                {category.name}
-                {
-                category.children.length>0 ?
-                    <ul>
-                        {renderCategories(category.children)}
-                    </ul>
-                    :
-                    null
-                }
-            </li>
-        )
+    for (let category of categories) {
+      rCategories.push(
+        <li key={category._id}>
+          {category.name}
+          {category.children.length > 0 ? (
+            <ul>{renderCategories(category.children)}</ul>
+          ) : null}
+        </li>
+      );
     }
     return rCategories;
-  }
+  };
+
+  const createCategoryList = (categories, options = []) => {
+    for (let category of categories) {
+      options.push({
+        name: category.name,
+        value: category._id,
+      });
+      if (category.children.length > 0) {
+        createCategoryList(category.children, options);
+      }
+    }
+    return options;
+  };
+
+  const submitHanlder = (e) => {
+    e.preventDefault();
+    const category = {
+        name,parentId,categoryImage
+    }
+    dispatch(addCategory(category));
+  };
+
   return (
     <Layout sidebar={true}>
-        <Row>
+      <Row>
+        <Col md={12}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              margin: "10px 0",
+              alignItems: "center",
+            }}
+          >
+            <h1>Category</h1>
+            <Button variant="primary" onClick={handleShow}>
+              Add New
+            </Button>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        {loading ? (
+          <h1
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Loading...
+          </h1>
+        ) : (
+          <Col md={12}>
+            {error ? (
+              <Alert variant="danger">{errorMessage}</Alert>
+            ) : (
+              renderCategories(categories)
+            )}
+          </Col>
+        )}
+        <CustomModal
+          show={show}
+          onHide={handleClose}
+          title="Add Category"
+          onSubmit={submitHanlder}
+        >
+          <Input
+            label="Enter Category Name"
+            placeholder="e.g. Mobile"
+            name="name"
+            type="name"
+            onChangeHandler={(e) => setName(e.target.value)}
+          />
 
-            <Col md={12}>
-                <div style={{
-                    display:"flex",
-                    justifyContent:'space-between',
-                    margin:"10px 0"
-                }}>
-                    <h1>Category</h1>
-                    <button>Add New</button>
-                </div>
-            </Col>
-        </Row>
-        <Row>
-            {
-                loading ? <h1 style={{
-                    textAlign:"center"
-                }}>Loading...</h1>
-            :
-            <Col md={12}>
-                {error ? 
-                <Alert variant="danger">
-                {errorMessage}
-              </Alert>
-                :
-                renderCategories(categories)
-                }
-            </Col>
-            }
-        </Row>
+          <Form.Select
+            onChange={(e) => {
+              setParentId(e.target.value);
+            }}
+          >
+            <option>Select Parent Category (If Any)</option>
+            {createCategoryList(categories).map(({ name, value }) => (
+              <option value={value}>{name}</option>
+            ))}
+          </Form.Select>
+          <Input
+            type="file"
+            name="categoryImage"
+            lable="Add Image"
+            onChangeHandler={(e)=>setCategoryImage(e.target.files[0])}
+          />
+        </CustomModal>
+      </Row>
     </Layout>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
